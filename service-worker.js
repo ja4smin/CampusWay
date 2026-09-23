212e653ebafd02153d358cd4898d2f02cc170aed
ja4smin <jasmin.taya@gmail.com>
2026-09-23 20:54:54 +0300

    Expand offline cache and fix indoor building switching

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'campusway-v3';
+const CACHE_NAME = 'campusway-v5';
 
 const APP_FILES = [
-  './',
+'./',
   './index.html',
   './manifest.json',
 
@@ -11,18 +11,56 @@ const APP_FILES = [
 
   './app/prototype/data.js',
   './app/prototype/outdoor-routing.js',
+  './app/prototype/campus-osm.json',
   './app/prototype/madriga-graph.js',
   './app/prototype/main-graph.js',
   './app/prototype/rabin-graph.js',
   './app/prototype/student-graph.js',
+  './app/prototype/multi-purpose-graph.js',
   './app/prototype/indoor.js',
 
-  './buildings/madriga/madriga-indoor-graph.json',
-    './buildings/main/main-indoor-graph.json',
-    './buildings/rabin/rabin-indoor-graph.json',
-    './buildings/student/student-indoor-graph.json',
+  './wayframe/navigation-demo.html',
 
-  './wayframe/navigation-demo.html'
+  './buildings/education/education-indoor-graph.json',
+  './buildings/education/floors/floor0.svg',
+  './buildings/education/floors/floor1.svg',
+  './buildings/education/floors/floor2.svg',
+  './buildings/education/floors/floor3.svg',
+  './buildings/education/floors/floor4.svg',
+  './buildings/education/floors/floor5.svg',
+  './buildings/education/floors/floor6.svg',
+
+  './buildings/madriga/madriga-indoor-graph.json',
+  './buildings/madriga/floors/floorminus1.svg',
+  './buildings/madriga/floors/floor0.svg',
+  './buildings/madriga/floors/floor1.svg',
+  './buildings/madriga/floors/floor2.svg',
+  './buildings/madriga/floors/floor3.svg',
+  './buildings/madriga/floors/floor4.svg',
+
+  './buildings/main/main-indoor-graph.json',
+  './buildings/main/floors/500-Model.svg',
+  './buildings/main/floors/600-Model.svg',
+  './buildings/main/floors/700-Model.svg',
+
+  './buildings/multi-purpose/multi-purpose-indoor-graph.json',
+  './buildings/multi-purpose/floors/floorminus1.svg',
+  './buildings/multi-purpose/floors/floor0.svg',
+  './buildings/multi-purpose/floors/floor1.svg',
+  './buildings/multi-purpose/floors/floor2.svg',
+
+  './buildings/rabin/rabin-indoor-graph.json',
+  './buildings/rabin/floors/floor5.svg',
+  './buildings/rabin/floors/floor6.svg',
+  './buildings/rabin/floors/floor7.svg',
+
+  './buildings/student/student-indoor-graph.json',
+  './buildings/student/floors/floor0.svg',
+  './buildings/student/floors/floor1.svg',
+  './buildings/student/floors/floor2.svg',
+  './buildings/student/floors/floor3.svg',
+  './buildings/student/floors/floor4.svg'
+ 
 ];
 
 self.addEventListener('install', event => {
